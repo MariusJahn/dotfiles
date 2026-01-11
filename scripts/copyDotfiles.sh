399e119fc257c761f7cbe1d166f7bd60920090b3
Marius Jahn <mariusjahn@web.de>
2026-01-11 17:36:56 +0100

    add copy script

---
+++ scripts/copyDotfiles.sh
@@ -0,0 +1,13 @@
+pathToRepository=~/ssd/repositories/dotfiles
+
+# zsh
+cp -r ${pathToRepository}/.config ~
+cp -r ${pathToRepository}/zsh/{.zshrc,.zshrc.work,.zshrc.personal} ~ 
+
+
+# Plasma
+#cp -r ${pathToRepository}/.config/plasma ~/plasma
+#cp -r ${pathToRepository}/.config/kdeglobals ~/.config/kdeglobals
+#cp -r ${pathToRepository}/.config/kwinrc ~/.config/kwinrc
+#cp -r ${pathToRepository}/.config/plasmarc ~/.config/plasmarc
+#cp -r ${pathToRepository}/.config/plasmashellrc ~/.config/plasmashellrc
